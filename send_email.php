bcc894b3791e723752845ef98c4c30b0639f581a
Trushita Das <163100739+trushita-288@users.noreply.github.com>
2024-03-13 18:03:09 +0530

    commit2

---
+++ send_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    $to = "trush.das288.com"; 
+    $email_subject = "New Contact Form Submission: $subject";
+    $email_body = "Name: $name\nEmail: $email\nSubject: $subject\nMessage:\n$message";
+    
+    if (mail($to, $email_subject, $email_body)) {
+        echo "Thank you for your message! We will get back to you soon.";
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+}
+?>
